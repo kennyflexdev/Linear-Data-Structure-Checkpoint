bceecc62b5562fef6ac330f35ae7d18fe9a5c232
kennyflexdev <kenchimezie56@gmail.com>
2022-10-17 22:10:14 -0700

    change data stucture

--- linear.js
+++ linear.js
@@ -3,11 +3,14 @@
 var k = [12, 13, 6, 10];
 var j = [13, 10, 16, 15];
 
+Newarr=[12,6,16,15]
+
 for(let item in k){
 for(let tools in j){
     if(item ==tools){
     new Array.push(item)
     }
+
     return newarr.reduce(sum,a)= sum+=a
 }    
 }
